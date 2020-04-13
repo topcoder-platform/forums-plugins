5c122a1b4e7488fcddd4c9443d3a594612173ff5
obog <o_bogdanova@inbox.ru>
2020-04-13 08:02:38 +0300

    Added the Topcoder plugin

---
+++ Topcoder/class.topcoder.plugin.php
@@ -0,0 +1,462 @@
+<?php
+/**
+ * Class TopcoderPlugin
+ */
+class TopcoderPlugin extends Gdn_Plugin {
+
+     /**
+     * Extra styling on the discussion view.
+     *
+     * @param \Vanilla\Web\Asset\LegacyAssetModel $sender
+     */
+    public function assetModel_styleCss_handler($sender) {
+        $sender->addCssFile('topcoder.css', 'plugins/Topcoder');
+    }
+
+    /**
+     * The settings page for the topcoder plugin.
+     *
+     * @param Gdn_Controller $sender
+     */
+    public function settingsController_topcoder_create($sender) {
+        $sender->permission('Garden.Settings.Manage');
+        $cf = new ConfigurationModule($sender);
+        $cf->initialize([
+            'Plugins.Topcoder.BaseApiURL' => ['Control' => 'TextBox', 'Default' => 'https://api.topcoder-dev.com', 'Description' => 'TopCoder Base API URL'],
+            'Plugins.Topcoder.MemberApiURI' => ['Control' => 'TextBox', 'Default' => '/v3/members', 'Description' => 'Topcoder Member API URI'],
+            'Plugins.Topcoder.MemberProfileURL' => ['Control' => 'TextBox', 'Default' => 'https://www.topcoder.com/members', 'Description' => 'Topcoder Member Profile URL'],
+        ]);
+
+        $sender->setData('Title', sprintf(t('%s Settings'), 'Topcoder'));
+        $cf->renderAll();
+    }
+
+    /**
+     * Use a Topcoder Photo on the user' profile.
+     *
+     * @param ProfileController $sender
+     * @param array $args
+     */
+    public function profileController_afterAddSideMenu_handler($sender, $args) {
+        $sender->User->Photo = userPhotoDefaultUrl($sender->User, ['Size' => 200]);
+    }
+
+
+    /**
+     * Get a Topcoder Member Profile
+     * @param $name vanilla user name
+     * @return null|string  photo url
+     */
+    public static function getTopcoderProfile($name) {
+        $topcoderMembersApiUrl = c('Plugins.Topcoder.BaseApiURL').c('Plugins.Topcoder.MemberApiURI');
+        $memberData = file_get_contents($topcoderMembersApiUrl.'/'.$name);
+        $memberResponse = json_decode($memberData);
+        //Use a photo of Topcoder member if the member with the given user name exists and photoUrl is not null
+        if($memberResponse->result->status === 200 && $memberResponse->result->content !== null) {
+            return  $memberResponse->result->content;
+        }
+        return null;
+    }
+
+    /**
+     * Get a photo url from Topcoder Member Profile
+     * @param $name vanilla user name
+     * @return null|string  photo url
+     */
+    public static function getTopcoderPhotoUrl($name) {
+        $topcoderProfile = self::getTopcoderProfile($name);
+        if($topcoderProfile !== null) {
+            return  $topcoderProfile->photoURL;
+        }
+        return null;
+    }
+
+
+    /**
+     * Get a Tocoder rating from Topcoder Member Statistics
+     * @param $name
+     * @return int|null
+     */
+    public static function getTopcoderRating($name) {
+        $topcoderMembersApiUrl = c('Plugins.Topcoder.BaseApiURL').c('Plugins.Topcoder.MemberApiURI');
+        $memberStatsData = file_get_contents($topcoderMembersApiUrl.'/'.$name.'/stats');
+        $memberStatsResponse = json_decode($memberStatsData);
+        if($memberStatsResponse->result->status === 200 && $memberStatsResponse->result->content[0]->maxRating) {
+            return $memberStatsResponse->result->content[0]->maxRating->rating;
+        }
+
+        return null;
+    }
+
+    /**
+     * Get css style based on Topcoder Member Rating
+     * @param $rating
+     * @return mixed|string
+     */
+    public static function getRatingCssClass($rating){
+        $cssStyles = array('coderTextOrange', 'coderTextWhite', 'coderTextGray',
+            'coderTextGreen', 'coderTextBlue', 'coderTextYellow', 'coderTextRed');
+
+        $cssStyle = '';
+        if ($rating < 0) {
+            $cssStyle = $cssStyles[0];
+        } else if ($rating == 0) {
+            $cssStyle = $cssStyles[1];
+        } else if ($rating > 0 && $rating < 900) {
+            $cssStyle = $cssStyles[2];
+        } else if ($rating > 899 && $rating < 1200) {
+            $cssStyle = $cssStyles[3];
+        } else if ($rating > 1199 && $rating < 1500) {
+            $cssStyle = $cssStyles[4];
+        } else if ($rating > 1499 && $rating < 2200) {
+            $cssStyle = $cssStyles[5];
+        } else if ($rating > 2199) {
+            $cssStyle = $cssStyles[6];
+        }
+
+        return $cssStyle;
+    }
+
+
+    public static function getUserPhotoUrl($user) {
+        $name = val('Name', $user, null);
+        if ($name !== null) {
+            $photoUrl = self::getTopcoderPhotoUrl($name);
+            return $photoUrl === null? UserModel::getDefaultAvatarUrl(): $photoUrl;
+        }
+        return UserModel::getDefaultAvatarUrl();
+    }
+}
+
+
+if (!function_exists('userBuilder')) {
+    /**
+     * Take an object & prefix value and convert it to a user object that can be used by userAnchor() && userPhoto().
+     *
+     * The object must have the following fields: UserID, Name, Photo.
+     *
+     * @param stdClass|array $row The row with the user extract.
+     * @param string|array $userPrefix Either a single string user prefix or an array of prefix searches.
+     * @return stdClass Returns an object containing the user.
+     */
+    function userBuilder($row, $userPrefix = '') {
+        $row = (object)$row;
+        $user = new stdClass();
+
+        if (is_array($userPrefix)) {
+            // Look for the first user that has the desired prefix.
+            foreach ($userPrefix as $px) {
+                if (property_exists($row, $px.'Name')) {
+                    $userPrefix = $px;
+                    break;
+                }
+            }
+
+            if (is_array($userPrefix)) {
+                $userPrefix = '';
+            }
+        }
+
+        $userID = $userPrefix.'UserID';
+        $name = $userPrefix.'Name';
+        $photo = $userPrefix.'Photo';
+        $gender = $userPrefix.'Gender';
+        $user->UserID = $row->$userID;
+        $user->Name = $row->$name;
+
+        $topcoderPhotoUrl = TopcoderPlugin::getTopcoderPhotoUrl($user->Name);
+        if($topcoderPhotoUrl !== null) {
+            $user->Photo = $topcoderPhotoUrl;
+            $user->PhotoUrl = $topcoderPhotoUrl;
+        } else {
+            $user->Photo = property_exists($row, $photo) ? $row->$photo : '';
+        }
+
+        $user->Email = val($userPrefix.'Email', $row, null);
+        $user->Gender = property_exists($row, $gender) ? $row->$gender : null;
+
+        return $user;
+    }
+}
+
+if (!function_exists('userPhoto')) {
+    /**
+     * Takes a user object, and writes out an anchor of the user's icon to the user's profile.
+     * Use a photoUrl from Topcoder profile
+     * @param object|array $user A user object or array.
+     * @param array $options
+     * @return string HTML.
+     */
+    function userPhoto($user, $options = []) {
+        if (is_string($options)) {
+            $options = ['LinkClass' => $options];
+        }
+
+        if ($px = val('Px', $options)) {
+            $user = userBuilder($user, $px);
+        } else {
+            $user = (object)$user;
+        }
+
+        $linkClass = concatSep(' ', val('LinkClass', $options, ''), 'PhotoWrap');
+        $imgClass = val('ImageClass', $options, 'ProfilePhoto');
+
+        $size = val('Size', $options);
+        if ($size) {
+            $linkClass .= " PhotoWrap{$size}";
+            $imgClass .= " {$imgClass}{$size}";
+        } else {
+            $imgClass .= " {$imgClass}Medium"; // backwards compat
+        }
+
+        $fullUser = Gdn::userModel()->getID(val('UserID', $user), DATASET_TYPE_ARRAY);
+        $userCssClass = val('_CssClass', $fullUser);
+        if ($userCssClass) {
+            $linkClass .= ' '.$userCssClass;
+        }
+
+        $photoUrl =  val('PhotoUrl', $user);
+        $name = val('Name', $fullUser);
+        $title = htmlspecialchars(val('Title', $options, $name));
+
+        if ($fullUser && $fullUser['Banned']) {
+            $title .= ' ('.t('Banned').')';
+        }
+
+        $attributes = [
+            'class' => $linkClass,
+            'rel' => val('Rel', $options)
+        ];
+
+        $userLink = userUrl($fullUser);
+        $topcoderProfile = TopcoderPlugin::getTopcoderProfile($name);
+        if($topcoderProfile !== null) {
+            $attributes['target'] = '_blank';
+
+            $topcoderMemberProfileUrl = c('Plugins.Topcoder.MemberProfileURL');
+            $userLink = $topcoderMemberProfileUrl . '/' . $name;
+            $topcoderPhotoUrl = $topcoderProfile->photoURL;
+            if ($topcoderPhotoUrl !== null) {
+                $photoUrl = $topcoderPhotoUrl;
+            }
+        }
+
+        $photoUrl = isset($photoUrl) && !empty(trim($photoUrl)) ? $photoUrl: UserModel::getDefaultAvatarUrl();
+        $href = (val('NoLink', $options)) ? '' : ' href="'.url($userLink).'"';
+
+        return '<a title="'.$title.'"'.$href.attribute($attributes).'>'
+            .img($photoUrl, ['alt' => $name, 'class' => $imgClass])
+            .'</a>';
+    }
+}
+
+if (!function_exists('userPhotoDefaultUrl')) {
+    /**
+     * Take a user object an return the URL to their photo.
+     *
+     * @param object|array $user
+     * @return string
+     */
+    function userPhotoDefaultUrl($user) {
+        return TopcoderPlugin::getUserPhotoUrl($user);
+    }
+}
+
+if (!function_exists('userPhotoUrl')) {
+    /**
+     * Take a user object an return the URL to their photo.
+     *
+     * @param object|array $user
+     * @return string
+     */
+    function userPhotoUrl($user) {
+        return TopcoderPlugin::getUserPhotoUrl($user);
+    }
+}
+
+if (!function_exists('userAnchor')) {
+    /**
+     * Take a user object, and writes out an anchor of the user's name to the user's profile.
+     *
+     * @param array|object $user
+     * @param null $cssClass
+     * @param null $options
+     * @return string
+     */
+    function userAnchor($user, $cssClass = null, $options = null) {
+        static $nameUnique = null;
+        if ($nameUnique === null) {
+            $nameUnique = c('Garden.Registration.NameUnique');
+        }
+
+        if (is_array($cssClass)) {
+            $options = $cssClass;
+            $cssClass = null;
+        } elseif (is_string($options)) {
+            $options = ['Px' => $options];
+        }
+
+        $px = val('Px', $options, '');
+        $name = val($px.'Name', $user, t('Unknown'));
+        $text = val('Text', $options, htmlspecialchars($name)); // Allow anchor text to be overridden.
+
+        $attributes = [
+            'class' => $cssClass,
+            'rel' => val('Rel', $options)
+          ];
+        if (isset($options['title'])) {
+            $attributes['title'] = $options['title'];
+        }
+
+        $userUrl = userUrl($user, $px);
+        $topcoderRating = TopcoderPlugin::getTopcoderRating($name);
+        if($topcoderRating != null) {
+            $coderStyles = TopcoderPlugin::getRatingCssClass($topcoderRating);
+            $attributes['class'] = $attributes['class'].' '.$coderStyles ;
+        }
+
+        return '<a href="'.htmlspecialchars(url($userUrl)).'"'.attribute($attributes).'>'.$text.'</a>';
+    }
+}
+
+if (!function_exists('writeActivity')) {
+    // The issue: writesActivity fires 'BeforeActivity' but the author link with the photo has been rendered.
+    // So profileController_BeforeActivity_handler couldn't help to overwrite the properties.
+    // This function is copied from \applications\dashboard\views\activity\helper_functions.php
+    // and used to render Activities on Profile page.
+    function writeActivity($activity, $sender, $session) {
+        $activity = (object)$activity;
+        // If this was a status update or a wall comment, don't bother with activity strings
+        $activityType = explode(' ', $activity->ActivityType); // Make sure you strip out any extra css classes munged in here
+        $activityType = $activityType[0];
+        $author = userBuilder($activity, 'Activity');
+        $photoAnchor = '';
+
+        if ($activity->Photo) {
+            //FIX: Use the photoUrl from the author
+            $photoAnchor = anchor(
+                img($author->Photo, ['class' => 'ProfilePhoto ProfilePhotoMedium', 'aria-hidden' => 'true']),
+                $activity->PhotoUrl, 'PhotoWrap');
+        }
+
+        $cssClass = 'Item Activity Activity-'.$activityType;
+        if ($photoAnchor != '')
+            $cssClass .= ' HasPhoto';
+
+        $format = val('Format', $activity);
+        if (!$format) {
+            $format = 'html';
+        }
+
+        $title = '';
+        $excerpt = Gdn_Format::to($activity->Story, $format);
+
+        if ($activity->NotifyUserID > 0 || !in_array($activityType, ['WallComment', 'WallPost', 'AboutUpdate'])) {
+            $title = '<div class="Title" role="heading" aria-level="3">'.Gdn_Format::to(val('Headline', $activity), 'html').'</div>';
+        } else if ($activityType == 'WallPost') {
+            $regardingUser = userBuilder($activity, 'Regarding');
+            $photoAnchor = userPhoto($regardingUser);
+            $title = '<div class="Title">'
+                .userAnchor($regardingUser, 'Name')
+                .' <span>&rarr;</span> '
+                .userAnchor($author, 'Name')
+                .'</div>';
+
+            if (!$format)
+                $excerpt = Gdn_Format::display($excerpt);
+        } else {
+            $title = userAnchor($author, 'Name');
+            if (!$format)
+                $excerpt = Gdn_Format::display($excerpt);
+        }
+        $sender->EventArguments['Activity'] = &$activity;
+        $sender->EventArguments['CssClass'] = &$cssClass;
+        $sender->fireEvent('BeforeActivity');
+        ?>
+    <li id="Activity_<?php echo $activity->ActivityID; ?>" class="<?php echo $cssClass; ?>">
+        <?php
+        if (ActivityModel::canDelete($activity)) {
+            echo '<div class="Options">'.anchor('&times;', 'dashboard/activity/delete/'.$activity->ActivityID.'/'.$session->transientKey().'?Target='.urlencode($sender->SelfUrl), 'Delete').'</div>';
+        }
+        if ($photoAnchor != '') {
+            ?>
+            <div class="Author Photo"><?php echo $photoAnchor; ?></div>
+        <?php } ?>
+        <div class="ItemContent Activity">
+            <?php echo $title; ?>
+            <?php echo wrapIf($excerpt, 'div', ['class' => 'Excerpt userContent']); ?>
+            <?php
+            $sender->EventArguments['Activity'] = $activity;
+            $sender->fireAs('ActivityController')->fireEvent('AfterActivityBody');
+
+            // Reactions stub
+            if (in_array(val('ActivityType', $activity), ['Status', 'WallPost']))
+                writeReactions($activity);
+            ?>
+            <div class="Meta">
+                <span class="MItem DateCreated"><?php echo Gdn_Format::date($activity->DateInserted); ?></span>
+                <?php
+                $sharedString = FALSE;
+                $iD = val('SharedNotifyUserID', $activity->Data);
+                if (!$iD)
+                    $iD = val('CommentNotifyUserID', $activity->Data);
+
+                if ($iD)
+                    $sharedString = formatString(t('Comments are between {UserID,you}.'), ['UserID' => [$activity->NotifyUserID, $iD]]);
+
+                $allowComments = $activity->NotifyUserID < 0 || $sharedString;
+
+
+                if ($allowComments && $session->checkPermission('Garden.Profiles.Edit')) {
+                    echo '<span class="MItem AddComment">'
+                        .anchor(t('Activity.Comment', 'Comment'), '#CommentForm_'.$activity->ActivityID, 'CommentOption')
+                        .'</span>';
+                }
+
+                if ($sharedString) {
+                    echo ' <span class="MItem"><i>'.$sharedString.'</i></span>';
+                }
+
+                $sender->fireEvent('AfterMeta');
+                ?>
+            </div>
+        </div>
+        <?php
+        $comments = val('Comments', $activity, []);
+        if (count($comments) > 0) {
+            echo '<ul class="DataList ActivityComments">';
+            foreach ($comments as $comment) {
+                writeActivityComment($comment, $activity);
+            }
+        } else {
+            echo '<ul class="DataList ActivityComments Hidden">';
+        }
+
+        if ($session->checkPermission('Garden.Profiles.Edit')):
+            ?>
+            <li class="CommentForm">
+                <?php
+                echo anchor(t('Write a comment'), '/dashboard/activity/comment/'.$activity->ActivityID, 'CommentLink');
+                $commentForm = Gdn::factory('Form');
+                $commentForm->setModel($sender->ActivityModel);
+                $commentForm->addHidden('ActivityID', $activity->ActivityID);
+                $commentForm->addHidden('Return', Gdn_Url::request());
+                echo $commentForm->open(['action' => url('/dashboard/activity/comment'), 'class' => 'Hidden']);
+                echo '<div class="TextBoxWrapper">'.$commentForm->textBox('Body', ['MultiLine' => true, 'value' => '']).'</div>';
+
+                echo '<div class="Buttons">';
+                echo $commentForm->button('Comment', ['class' => 'Button Primary']);
+                echo '</div>';
+
+                echo $commentForm->close();
+                ?></li>
+        <?php
+        endif;
+
+        echo '</ul>';
+        ?>
+        </li>
+        <?php
+    }
+}
+
